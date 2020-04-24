c48109ee8f532a42012ce42268d3b2b4bc0e3618
akshay kalucha <40710016+akshaykalucha3@users.noreply.github.com>
2020-04-25 00:05:20 +0530

    Update Whatsapp.py

--- Whatsapp.py
+++ Whatsapp.py
@@ -1,16 +1,16 @@
 from twilio.rest import Client 
 # from Tracker import converted_price
  
-account_sid = 'AC28aae9205d1514633e8ea129436a9bb6' 
-auth_token = '7cb6c719f1b57f49651250da2fc50954' 
+account_sid = [YOUR_TWILIO_SID] 
+auth_token = [YOUR_TWILIO_AUTH_TOKEN]
 client = Client(account_sid, auth_token) 
 
 def send_msg(price):
 
     message = client.messages.create( 
-                                from_='whatsapp:+14155238886',  
+                                from_='whatsapp:[AUTH_NUMBER],  
                                 body=price,      
-                                to='whatsapp:+919873835100'
+                                to='whatsapp:+[RECIEVERS_NUMBER]
                             ) 
     
     print(message.sid)
