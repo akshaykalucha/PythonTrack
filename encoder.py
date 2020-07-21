d63efeb600826843e13b93bf5cc8605b5f04a314
akshay kalucha <akshaykalucha@gmail.com>
2020-07-21 20:15:19 +0530

    update'

--- encoder.py
+++ encoder.py
@@ -1,36 +1,57 @@
-alphabetList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-'u', 'v', 'w', 'x', 'y', 'z']
+# alphabetList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+# 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-reversedlist = []
+# reversedlist = []
 
-for i in range( len(alphabetList) - 1, -1, -1):
-    reversedlist.append(alphabetList[i])
-
-
-string = input()
-strList = []
-
-for i in range(len(string)):
-    strList.append(string[i])
-
-i = 0
-while i < strList.__len__() - 1:
-    t = strList[i]
-    strList[i] = strList[i + 1]
-    strList[i + 1] = t
-    i = i + 2
+# for i in range( len(alphabetList) - 1, -1, -1):
+#     reversedlist.append(alphabetList[i])
 
 
-# print(strList)
+# string = input()
+# strList = []
 
-newLst = []
+# for i in range(len(string)):
+#     strList.append(string[i])
 
-for j in range(len(strList)):
-    for k in range(len(alphabetList)):
-        if strList[j] == alphabetList[k]:
-            # print(strList[j], reversedlist[k])
-            newLst.append(reversedlist[k])
-            break
+# i = 0
+# while i < strList.__len__() - 1:
+#     t = strList[i]
+#     strList[i] = strList[i + 1]
+#     strList[i + 1] = t
+#     i = i + 2
 
-print(*newLst, sep='')
+
+# # print(strList)
+
+# newLst = []
+
+# for j in range(len(strList)):
+#     for k in range(len(alphabetList)):
+#         if strList[j] == alphabetList[k]:
+#             # print(strList[j], reversedlist[k])
+#             newLst.append(reversedlist[k])
+#             break
+
+# print(*newLst, sep='')
+
+L = 1000000000123
+R = 1000000123456
+if R - L == 1:
+    print(R)
+else:
+    globarr = []
+    for i in range(L, R+1):
+        bitarr = []
+        for j in range(L, R+1):
+            if i == j:
+                bitor = i
+                bitarr.append(bitor)
+            else:
+                bitor = i | j
+                bitarr.append(bitor)
+        globarr.append(max(bitarr))
+        # print(bitarr)
+        # break
+    
+    print(max(globarr))
