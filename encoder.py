a07f4b74c0b67b10ecb28bdb251f2091bc2d300a
akshay kalucha <akshaykalucha@gmail.com>
2020-10-12 16:16:57 +0530

    updates

--- encoder.py
+++ encoder.py
@@ -258,3 +258,15 @@ if __name__ == '__main__':
     totient = Totient(10**18)
     print totient
     #print sum(imap(totient, range(10**18)))
+
+
+  
+import sys
+if __name__=='__main__':
+    xx=[0]*(10**9+2)
+    for i in range(1000):
+        for j in range(1000):
+            p=i*i+j*j
+            if p<=10**9:
+                xx[p]=1
+    print xx
