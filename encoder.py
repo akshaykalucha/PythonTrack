d902aad2e5e5ade6c894e463db45e98eafd69d03
akshay kalucha <akshaykalucha@gmail.com>
2020-10-12 16:15:27 +0530

    CP updates

--- encoder.py
+++ encoder.py
@@ -220,3 +220,20 @@ def main():
         print s
 if __name__=='__main__':
     main()
+
+
+import sys
+def ans(n,b,i,val):
+    if n==i or n==0:
+        return val+i
+    if b==val+i:
+        return ans(n,b,i+1,val)
+    else:
+        return ans(n,b,i+1,val+i)
+def main():
+    t=int(sys.stdin.readline())
+    for i in range(t):
+        n,b=sys.stdin.readline().split()
+        print ans(int(n),int(b),0,0)
+if __name__=='__main__':
+    main()
