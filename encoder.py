10bdae3f925671c4857b3d4bbedd78f25208c14e
akshay kalucha <akshaykalucha@gmail.com>
2020-10-12 16:14:56 +0530

    Radix

--- encoder.py
+++ encoder.py
@@ -159,3 +159,64 @@ def subarrayGivenSum(lst, S):
     return -1
 z = subarrayGivenSum(lst, subsum)
 print(z)
+
+
+
+import sys
+def getQ(size):
+    bucket=[[] for i in range(size)]
+    return bucket
+def noDigit(b):
+    return len(str(max(b)))
+def getHash(val,i):
+    i+=1
+    return (val%10**i)/(10**(i-1))
+def radix(b,i):
+    bucket=getQ(10)
+    for elem in b:
+        p=getHash(elem,i)
+        bucket[p].append(elem)
+    dic=[]
+    for everylist in bucket:
+        dic.extend(everylist)
+    return dic
+def SortIt(b):
+    noofd=noDigit(b)
+    for i in range(noofd):
+        b=radix(b,i)
+    return b
+if __name__=='__main__':
+    print 'Enter The Numbers'
+    a=sys.stdin.readline().split()
+    b=[int(i) for i in a]
+    b=SortIt(b)
+    for i in b:
+        print i
+
+
+def main():
+    N=(int)(input())
+    abc=0;
+    for akjhdkafka in range(N):
+        abc+=1
+        s="Case #"+str(abc)+": "
+        C=(int)(input())
+        I=(int)(input())
+        A=raw_input().split(' ')
+        P=[]
+        for x in range(len(A)):
+            P.append((int)(A[x]))
+        a=-1
+        b=-1
+        for x in range(len(P)):
+            if C-P[x] in P:
+                a=x;
+                b=P.index(C-P[x],x);
+                break;
+        if(a>b):
+            s+=str(b+1)+' '+str(a+1)
+        else:
+            s+=str(a+1)+' '+str(b+1)
+        print s
+if __name__=='__main__':
+    main()
