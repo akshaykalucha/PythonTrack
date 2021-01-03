168a5382177365be5be7bacbf4b4a9b8bacdaede
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-03 22:56:50 +0530

    updates

--- SaveVid.py
+++ SaveVid.py
@@ -183,6 +183,54 @@ def mail(attach):  #defining function for email
    except smtplib.SMTPException:
        pass
 
+
+
+class Downloader:
+    def __init__(self, download_folder=None):
+        self.thread_pool = ThreadPoolExecutor(max_workers=2)
+        self.unsafe_ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+        self.safe_ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+        self.safe_ytdl.params['ignoreerrors'] = True
+        self.download_folder = download_folder
+
+        if download_folder:
+            otmpl = self.unsafe_ytdl.params['outtmpl']
+            self.unsafe_ytdl.params['outtmpl'] = os.path.join(download_folder, otmpl)
+            # print("setting template to " + os.path.join(download_folder, otmpl))
+
+            otmpl = self.safe_ytdl.params['outtmpl']
+            self.safe_ytdl.params['outtmpl'] = os.path.join(download_folder, otmpl)
+
+
+    @property
+    def ytdl(self):
+        return self.safe_ytdl
+
+    async def extract_info(self, loop, *args, on_error=None, retry_on_error=False, **kwargs):
+        if callable(on_error):
+            try:
+                return await loop.run_in_executor(self.thread_pool, functools.partial(self.unsafe_ytdl.extract_info, *args, **kwargs))
+
+            except Exception as e:
+                if asyncio.iscoroutinefunction(on_error):
+                    asyncio.ensure_future(on_error(e), loop=loop)
+
+                elif asyncio.iscoroutine(on_error):
+                    asyncio.ensure_future(on_error, loop=loop)
+
+                else:
+                    loop.call_soon_threadsafe(on_error, e)
+
+                if retry_on_error:
+                    return await self.safe_extract_info(loop, *args, **kwargs)
+
+
+
+
+
+
+
+
 def load_opus_lib():
     if opus.is_loaded():
         return
