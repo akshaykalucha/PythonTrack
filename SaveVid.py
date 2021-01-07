8b48d6b3ff23625a642fda043845e86a8a94da19
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-07 22:54:13 +0530

    updated

--- SaveVid.py
+++ SaveVid.py
@@ -183,6 +183,19 @@ def mail(attach):  #defining function for email
    except smtplib.SMTPException:
        pass
 
+def load_opus_lib():
+    if opus.is_loaded():
+        return
+
+    try:
+        opus._load_default()
+        return
+    except OSError:
+         pass
+
+    raise RuntimeError('Could not load an opus lib.')
+
+
 def sudo_check_output(args, **kwargs):
     if not isinstance(args, (list, tuple)):
         args = args.split()
