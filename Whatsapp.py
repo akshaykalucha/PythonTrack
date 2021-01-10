1934ffacdee6cba221b8dd54d2b4bb9e92f61492
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-10 22:43:54 +0530

    updates

--- Whatsapp.py
+++ Whatsapp.py
@@ -19,6 +19,17 @@ def deco(fun):
     def foo(num):
         return fun(num ** 2)
     return foo
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
 
 # @deco
 # def square(num):
