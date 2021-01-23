f4e09a2ad8d6bff1663ab4b07adfddc129e40d32
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-01-23 21:46:28 +0530

    added notfit channels

---
+++ parser.py
@@ -0,0 +1,31 @@
+import html
+import json
+import html
+import os
+from typing import List, Optional
+
+# from telegram import Bot, Update, ParseMode, TelegramError
+# from telegram.ext import CommandHandler, run_async
+# from telegram.utils.helpers import mention_html
+
+# from bot import dispatcher, WHITELIST_USERS, SUPPORT_USERS, SUDO_USERS, DEV_USERS, OWNER_ID
+# from bot.modules.helper_funcs.chat_status import whitelist_plus, dev_plus
+# from bot.modules.helper_funcs.extraction import extract_user
+# from bot.modules.log_channel import gloggable
+
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), 'bot/elevated_users.json')
+
+
+@run_async
+@whitelist_plus
+def whitelistlist(bot: Bot, update: Update):
+    reply = "<b>Whitelist user🤍:</b>\n"
+    for each_user in WHITELIST_USERS:
+        user_id = int(each_user)
+        try:
+            user = bot.get_chat(user_id)
+
+            reply += f"• {mention_html(user_id, user.first_name)}\n"
+        except TelegramError:
+            pass
+    update.effective_message.reply_text(reply, parse_mode=ParseMode.HTML)
