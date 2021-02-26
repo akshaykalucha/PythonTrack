7e61b9bb68b27dbab6a93d9a26551239fdebba21
akshay kalucha <akshaykalucha@gmail.com>
2021-02-26 21:58:17 +0530

    dataset generated and added

--- Dataset.py
+++ Dataset.py
@@ -30,3 +30,41 @@ class FingerprintsDataset(Dataset):
         """Return dataset length."""
         return self.labels.shape[0]
 
+    def __getitem__(self, idx: int) -> Tuple[Any, Any]:
+        """Return the idx dataset element."""
+        return self.fingerprints[idx], self.labels[idx]
+
+
+class MolGraphDataset(tg.data.Dataset):
+    """Dataset for molecular graphs."""
+
+    def __init__(self, root: str, data: pd.DataFrame, properties: List[str] = None):
+        """Generate Molecular graph dataset."""
+        super().__init__(root)
+        self.molecules = data['molecules']
+        self.molecules.reset_index(drop=True, inplace=True)
+        self.norm = tg.transforms.NormalizeFeatures()
+        if properties is not None:
+            self.labels = data[properties].to_numpy(np.float32)
+        else:
+            self.labels = None
+
+    def _download(self):
+        pass
+
+    def _process(self):
+        pass
+
+    def __len__(self):
+        """Return dataset length."""
+        return len(self.molecules)
+
+    def __getitem__(self, idx):
+        """Return the idx dataset element."""
+        if self.labels is not None:
+            # labels = torch.Tensor([self.labels[idx]]).reshape(1, 1)
+            labels = torch.Tensor([self.labels[idx]])
+        else:
+            labels = None
+        data = create_molecular_graph_data(self.molecules[idx], labels)
+        return self.norm(data)
