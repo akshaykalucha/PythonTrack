46d5f61d25ea8c6cc5c09799a6e9e77981a7146e
akshay kalucha <akshaykalucha@gmail.com>
2021-02-27 22:11:15 +0530

    dataset generated and added

--- ImageSearch.py
+++ ImageSearch.py
@@ -2,6 +2,23 @@ from utils.media_sender import ImageSender
 import requests, urllib
 import config
 
+def getwords(doc):
+    splitter = re.compile('\\W*')
+    
+    # Split the words by non-alpha characters
+    words = [s.lower() for s in splitter.split(doc)
+               if len(s)>2 and len(s)<20]
+    
+    # Return the unique set of words only
+    return dict([(w,1) for w in words])
+
+def sampletrain(cl):
+    cl.train('Nobody owns the water.','good')
+    cl.train('the quick rabbit jumps fences','good')
+    cl.train('buy pharmaceuticals now','bad')
+    cl.train('make quick money at the online  casino','bad')
+    cl.train('the quick brown fox jumps','good')
+
 class BingViews():
     def __init__(self, interface_layer):
         self.image_sender = ImageSender(interface_layer)
