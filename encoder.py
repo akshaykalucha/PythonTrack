886fe5cd35542e3d49b5703f8d2d0da36cb18719
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-03-24 23:25:58 +0530

    LL FIFO

--- encoder.py
+++ encoder.py
@@ -483,3 +483,16 @@ l1.printLL()
 l1.remove()
 print("After Removal : ")
 l1.printLL()
+
+
+def findConsecutiveZeros(list1):
+    	count = 0
+	for i in range(len(list1) - 4):
+		if list1[i] == 0:
+			if (list1[i+1] == 0 and list1[i+2] == 0 and list1[i+3] == 0 and list1[i+4] == 0):
+				count += 1
+
+	print("This boolean array has {0} instances of 5 consecutive 0s".format(count))
+
+nums = [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+findConsecutiveZeros(nums)
