6f9fccac746260d8dd8a808f988692cb27f8e1db
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-03-26 23:22:11 +0530

    create main function

--- ImageSearch.py
+++ ImageSearch.py
@@ -2,6 +2,45 @@ from utils.media_sender import ImageSender
 import requests, urllib
 import config
 
+
+
+class EvaluateChoose(object):
+    def __init__(self, models_path, configuration_path):
+        self.models_path = models_path
+        self.configuration_path = configuration_path
+
+    def evaluation(self, validation_data, validation_labels):
+        # This function takes validation data and labels and evaluate them
+        # Evaluation results are gathered in list which is saved to txt file. This saving is done because of preventing
+        # evaluate it every running.
+        # Returns results of evaluation which is list of tuples. Each tuple has such shape (model_name, acc, loss).
+        results = []
+
+        file = 'results_of_models.json'
+        file_path = os.path.join(self.configuration_path, file)
+        if os.path.exists(file_path):
+            with open(file_path) as results_file:
+                results_dict = json.load(results_file)
+            results_str = results_dict['results']
+            for each in results_str:
+                results.append((each[0], float(each[1]), float(each[2])))
+            print('Results were loaded from {}!'.format(self.configuration_path))
+        else:
+            models = os.listdir(self.models_path)
+            for each in models[:-3]:
+                print('{} model is loaded from the {} folder.'.format(each, self.models_path))
+                model_path = os.path.join(self.models_path, each)
+                model = tensorflow.keras.models.load_model(model_path)
+                loss, acc = model.evaluate(validation_data, validation_labels, verbose=2)
+                results.append((each, str(acc), str(loss)))
+            results_dict = {'results': results}
+            with open(file_path, 'w') as results_file:
+                json.dump(results_dict, results_file)
+            print('Results were saved to {}!'.format(self.configuration_path))
+        return results
+
+
+
 def getwords(doc):
     splitter = re.compile('\\W*')
     
