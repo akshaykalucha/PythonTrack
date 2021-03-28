b4ee3391200321df5080a65a76e3856c5fa406c5
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-03-28 23:20:02 +0530

    image search fetch class

--- ImageSearch.py
+++ ImageSearch.py
@@ -113,6 +113,27 @@ root.mainloop()
 
 
 
+epochs = 100
+print('VGG16 will be trained in {} combinations on the following dataset: {}.'.format(len(models), augmentation_of_max))
+train_data_raw, train_labels_raw = rd_train.load_augmented_dataset(augmentation_of_max)
+
+
+for each in models.keys():
+    model_params = models[each]
+    print('Transfer Learning model has chosen as {} version of ResNet'.format(each))
+    print('Model parameters will be: Unfreezed: {}, Weights: {}, Trainable: {}'.format(model_params[0], model_params[1],
+                                                                                       model_params[2]))
+
+    transfer_learning_model = TransferModel(epochs, input_shape, models_path, defrosted=model_params[0],
+                                            weights=model_params[1], trainable=model_params[2])
+    history = transfer_learning_model.model(train_data_raw, train_labels_raw, validation_dataset, model_name=each)
+
+    path_image_accuracy = os.path.join(results_path, each + '_accuracy.png')
+    path_image_loss = os.path.join(results_path, each + '_loss.png')
+
+    plot_images_results(path_image_accuracy, history, 'accuracy')
+    plot_images_results(path_image_loss, history, 'loss')
+
 
 
 class fisherclassifier(classifier):
