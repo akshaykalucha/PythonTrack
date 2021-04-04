7e8885a244d1198229af3f6f9ed96c7d5c426926
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-04-04 12:43:24 +0530

    added store

--- parser.py
+++ parser.py
@@ -70,3 +70,36 @@ def supportlist(bot: Bot, update: Update):
     else:
         message.reply_text("This user is not a whitelist!")
         return ""
+
+
+def search(title="", author="", year="", isbn=""):       # To pass some empty strings as default values. 
+    # (="") Even thougt you input only one value of one parameter, the system will not be error.
+    conn=sqlite3.connect("books.db")       #build a connect to the database
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))       
+    # select all the books where title equals to something and others are the same.
+    # (title, author, year, isbn) is the second parameter of the argument.
+    rows=cur.fetchall()       #a row function to return the tuple
+    conn.close() 
+    return rows
+
+def delete(id):  # delete the tuple of data by it's ID
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))            # Don't forget the comma
+    conn.commit()
+    conn.close() 
+
+def update(id, title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn, id))  # 'id' is at the end of the tuple
+    conn.commit()
+    conn.close() 
+
+connect()
+#insert("The Sun","Andy Wang",1918,9131231132) 
+#delete(7)
+update(5, "The moon","John Smooth",1917,99999)
+print(view())
+print(search(author="John Smith"))
