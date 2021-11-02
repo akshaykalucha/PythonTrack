4e56d5bb311a23607f58288502081c1155918c67
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-11-02 16:13:18 +0530

    add

--- edgedtct/rotation.py
+++ edgedtct/rotation.py
@@ -9,5 +9,9 @@ img = cv2.imread('linkedinDP.jpg')
 
 lin_res = cv2.resize(img, None, fx=5.5, fy=5.5, interpolation=cv2.INTER_CUBIC)
 
-cv2.imshow("mm", lin_res)
-cv2.waitKey(0)
+# translation
+# translation matrix(M)
+
+matrix = np.float32([[1,0,50], [0,1,100]])
+# apply M to the image
+translated = cv2.warpAffine(img, matrix, (img.shape[1]+100, img.shape[0]+100))
